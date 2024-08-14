1d1fe9385f07066a5f442c49b7badebd76878e80
Stepan <yakyben@yandex.by>
2024-08-14 18:59:06 +0300

    add Lesson12.java

---
+++ src/exception/MyArrayDataException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class MyArrayDataException extends Exception {
+    private int row;
+    private int column;
+    public MyArrayDataException (int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getColumn() {
+        return column;
+    }
+}
