946c7945aec8685679ebce85966a1973ac6c856d
Stepan <yakyben@yandex.by>
2024-08-25 17:10:55 +0300

    Commit Lesson7

---
+++ src/main/java/factorialTNG.java
@@ -0,0 +1,9 @@
+public class factorialTNG {
+    public static int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
---
+++ src/test/java/factorialTNGTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+public class factorialTNGTest {
+    @org.testng.annotations.Test
+    public void testCalculateFactorial() {
+        factorialTNG calculator = new factorialTNG();
+
+        Assertions.assertEquals(calculator.calculateFactorial(4), 24);
+        Assertions.assertEquals(calculator.calculateFactorial(1), 1);
+        Assertions.assertEquals(calculator.calculateFactorial(5), 120);
+    }
+}   
