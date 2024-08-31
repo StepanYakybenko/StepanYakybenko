278bf5d1a3e8ca21124205796c8443f2fba06e0b
Stepan <yakyben@yandex.by>
2024-08-31 14:41:27 +0300

    add Lesson13.java

--- src/lesson13/Capital.java
+++ src/lesson13/Capital.java
@@ -8,14 +8,12 @@ public class Capital {
                 "Tbilisi", "Istanbul", "Yerevan", "Moskow", "Minsk", "Rome", "Bucharest", "Sophia");
         System.out.println("Перечень городов:");
         capital.forEach(System.out::println);
-
         System.out.println("\nСтолицы:");
         Map<String, Integer> wordsCount = new HashMap<>();
         for (String s : capital) {
             wordsCount.put(s, wordsCount.getOrDefault(s, 0) + 1);
         }
         wordsCount.entrySet().forEach(System.out::println);
-
         System.out.println("\nБез повторов: ");
         Set<String> unique = new HashSet<>(capital);
         unique.forEach(System.out::println);
